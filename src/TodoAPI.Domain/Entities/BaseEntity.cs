39bd954dfc256c31be890b5655bae977d7bdcb73
Douglas Vitor <douglasvitor.dev@gmail.com>
2021-10-24 14:28:57 -0300

    Modelling domain specifications

---
+++ src/TodoAPI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TodoApi.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/TodoAPI.Domain/Entities/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TodoApi.Domain.Entities
+{
+    public class Todo : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsFinished { get; set; }
+
+        public TimeSpan ElapsedTime() => CreatedAt - DateTime.UtcNow;
+    }
+}
